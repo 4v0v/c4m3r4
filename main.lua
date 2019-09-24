338d3056feb65cecf7adb7a81b6c3c2a5fc97fd5
4v0v <harold0frere@gmail.com>
2019-09-24 16:50:16 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,6 +9,9 @@ function love.load()
     cam1 = Camera:new(0, 0, W/2, H, 0, 0.5)
     cam2 = Camera:new(W/2, 0, W/2, H/2, 0.2, 1)
     cam3 = Camera:new(W/2,  H/2, W/2, H/2, 0.2, 0.2)
+
+    cam1:setMode("smooth")
+    cam1:setScaleMode("smooth")
 end
 
 function love.update(dt)
@@ -16,20 +19,25 @@ function love.update(dt)
     cam2:update(dt)
     cam3:update(dt)
 
-    if love.keyboard.isDown("left")  then cam1:move(-10 , 0) end
-    if love.keyboard.isDown("right") then cam1:move(10 , 0) end    
-    if love.keyboard.isDown("up")    then cam1:move(0, 10) end
-    if love.keyboard.isDown("down")  then cam1:move(0, -10) end
+    if love.keyboard.isDown("left")  then cam1:attach(cam1:getTargetX() + -500 * dt, _) end
+    if love.keyboard.isDown("right") then cam1:attach(cam1:getTargetX() + 500 * dt , _) end    
+    if love.keyboard.isDown("up")    then cam1:attach(_ , cam1:getTargetY() + -500 * dt) end
+    if love.keyboard.isDown("down")  then cam1:attach(_ , cam1:getTargetY() + 500 * dt) end
 
-    if love.keyboard.isDown("a") then cam1:zoom(0.01) end
-    if love.keyboard.isDown("q") then cam1:zoom(-0.01) end
+    if love.keyboard.isDown("a") then cam1:zoom(2) end
+    if love.keyboard.isDown("q") then cam1:zoom(0.5) end
 
-    if love.keyboard.isDown("w") then cam1:rotate(0.01) end
-    if love.keyboard.isDown("x") then cam1:rotate(-0.01) end
+    if love.keyboard.isDown("x") then cam1:setAngle(dt) end
+    if love.keyboard.isDown("w") then cam1:setAngle(-dt) end
 
     if love.keyboard.isDown("1") then cam1:shake(50) end
     if love.keyboard.isDown("2") then cam1:shake(_, 1) end
-    if love.keyboard.isDown("3") then cam1:shake(_, _, 1 ) end
+    if love.keyboard.isDown("3") then cam1:shake(_, _, 1) end
+
+    if love.keyboard.isDown("4") then cam1:setPosition(0, 0) end
+    if love.keyboard.isDown("5") then cam1:attach(-cam1:getX(), -cam1:getY()) end
+
+    if love.keyboard.isDown("escape") then love.event.quit() end    
 end
 
 function love.draw()
